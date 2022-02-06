0bf2c8847e346f96ad9d6eaed474e4231e1cc8fa
Viktor Ebuka <58702989+MegatronB@users.noreply.github.com>
2022-02-06 10:07:47 +0100

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,25 +1,25 @@
+--QUERING THE TABLE FossilFuelConsuption
+
 SELECT *
 FROM [Data Exploration project]..FossilFuelConsuption 
 order by 2,3
 
---SELECT *
---FROM [Data Exploration project]..['fossil-fuels-per-capita$'] 
---order by 2,3
-
-SELECT Entity, Year, [Coal Consumption - TWh] 
-FROM [Data Exploration project]..FossilFuelConsuption 
-WHERE Entity ='Africa' and Year between 2000 and 2014
-order by [Coal Consumption - TWh] asc
-
-SELECT Entity, Year, [Coal Consumption - TWh]
-FROM [Data Exploration project]..FossilFuelConsuption 
-WHERE (Entity ='Africa' OR Entity='China') and Year between 2000 and 2014
-order by [Coal Consumption - TWh] asc
-
---ROUNDING VALUES OF A DATASET TO THE NEAREST NUMBER
+--ROUNDING OFF DATASET TO THE NEAREST NUMBER
 
 --SELECT Entity, Year, [Coal Consumption - TWh] 
 --FROM [Data Exploration project]..FossilFuelConsuption 
 --WHERE (Entity ='Africa' OR Entity='China') and Year between 2000 and 2014
 --order by [Coal Consumption - TWh] asc 
 SELECT ROUND(1234.6234, 3);
+
+--GROUPING BASED ON REGION
+
+
+SELECT Entity, Year, [Gas Consumption - TWh], [Coal Consumption - TWh], [Oil Consumption - TWh]
+FROM [Data Exploration project]..FossilFuelConsuption 
+WHERE (Entity ='Russia' OR Entity='China') and Year between 2000 and 2014
+UNION
+SELECT Entity, Year, [Gas Consumption - TWh], [Coal Consumption - TWh], [Oil Consumption - TWh]
+FROM [Data Exploration project]..FossilFuelConsuption 
+WHERE (Entity ='European Union' OR Entity='Central America') and Year between 2000 and 2014
+order by Year
